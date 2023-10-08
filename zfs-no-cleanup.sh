bd6f671a5b51f049d80245bb9b5d5031267e601f
Marcin Kordonski <kordonskim@gmail.com>
2023-10-07 21:59:50 -0500

    change file names

--- zfs-no-cleanup.sh
+++ zfs-no-cleanup.sh
@@ -1,18 +1,25 @@
-GRN='\033[0;32m'
-NC='\033[0m'
-
-
-# Adding zfs packages
-echo -e "\n${GRN}Adding zfs packages...${NC}\n"
-
-curl -s https://raw.githubusercontent.com/eoli3n/archiso-zfs/master/init | bash
-
 # find /dev/disk/by-id/
-
 # mount -o remount,size=1G /run/archiso/cowspace    
 # wipefs -a /dev/disk/by-id/ata-Hitachi_HDS5C3020BLE630_MCE7215P035WTN
+# git clone https://github.com/kordonskim/zfs-scripts
 
-echo -e "\n${GRN}Setting variables...${NC}\n"
+GRN='\033[0;32m'
+NC='\033[0m'
+BBLU='\033[1;34m'
+BRED='\033[1;31m'
+
+# Adding zfs packages
+# echo -e "\n${GRN}Adding zfs packages...${NC}\n"
+
+# curl -s https://raw.githubusercontent.com/eoli3n/archiso-zfs/master/init | bash
+
+#Increase cowspace to half of RAM.
+echo -e "\n${GRN}Increase cowspace to half of RAM...${NC}\n"
+
+mount -o remount,size=50% /run/archiso/cowspace
+
+# Setting variables
+echo -e "\n${GRN}Set variables...${NC}\n"
 
 DISK='/dev/disk/by-id/ata-Hitachi_HDS5C3020BLE630_MCE7215P035WTN'
 MNT=/mnt
@@ -20,7 +27,7 @@ SWAPSIZE=16
 RESERVE=1
 
 # create partitions
-echo -e "\n${GRN}Creating partitions...${NC}\n"
+echo -e "\n${GRN}Create partitions...${NC}\n"
 
 partition_disk () {
  local disk="${1}"
@@ -28,15 +35,16 @@ partition_disk () {
 
  parted --script --align=optimal  "${disk}" -- \
  mklabel gpt \
+ mkpart BIOS 1MiB 2MiB \
  mkpart EFI 2MiB 1GiB \
  mkpart bpool 1GiB 5GiB \
  mkpart rpool 5GiB -$((SWAPSIZE + RESERVE))GiB \
  mkpart swap  -$((SWAPSIZE + RESERVE))GiB -"${RESERVE}"GiB \
- mkpart BIOS 1MiB 2MiB \
- set 1 esp on \
- set 5 bios_grub on \
- set 5 legacy_boot on
-
+ set 1 bios_grub on \
+ set 1 legacy_boot on \
+ set 2 esp on \
+ set 5 swap on
+ 
  partprobe "${disk}"
 }
 
@@ -48,8 +56,8 @@ done
 echo -e "\n${GRN}Swap setup...${NC}\n"
 
 for i in ${DISK}; do
-   mkswap "${i}"-part4
-   swapon "${i}"-part4
+   mkswap "${i}"-part5
+   swapon "${i}"-part5
 done
 
 
@@ -87,7 +95,7 @@ zpool create -d \
     -R "${MNT}" \
     bpool \
     $(for i in ${DISK}; do
-       printf '%s ' "${i}-part2";
+       printf '%s ' "${i}-part3";
       done)
 
 # create rpool      
@@ -108,81 +116,79 @@ zpool create \
     -O mountpoint=/ \
     rpool \
    $(for i in ${DISK}; do
-      printf '%s ' "${i}-part3";
+      printf '%s ' "${i}-part4";
      done)
 
-#  create rpool system container
-echo -e "\n${GRN}Create rpool system container...${NC}\n"
+# Create system and user datasets
+echo -e "\n${GRN}Create system and user datasets...${NC}\n"
 
-zfs create \
- -o canmount=off \
- -o mountpoint=none \
-rpool/archlinux     
-
-# Create system datasets, manage mountpoints with mountpoint=legacy
-echo -e "\n${GRN}Create system datasets, manage mountpoints with legacy...${NC}\n"
-
+zfs create -o canmount=off -o mountpoint=none rpool/archlinux     
 zfs create -o canmount=noauto -o mountpoint=/  rpool/archlinux/root
 zfs mount rpool/archlinux/root
-zfs create -o mountpoint=legacy rpool/archlinux/home
-mkdir "${MNT}"/home
-mount -t zfs rpool/archlinux/home "${MNT}"/home
-# zfs create -o mountpoint=legacy  rpool/archlinux/var
-# zfs create -o mountpoint=legacy rpool/archlinux/var/lib
-# zfs create -o mountpoint=legacy rpool/archlinux/var/log
+
+zfs create -o mountpoint=/home rpool/archlinux/home
+
 zfs create -o mountpoint=none bpool/archlinux
-zfs create -o mountpoint=legacy bpool/archlinux/root
-mkdir "${MNT}"/boot
-mount -t zfs bpool/archlinux/root "${MNT}"/boot
-# mkdir -p "${MNT}"/var/log
-# mkdir -p "${MNT}"/var/lib
-# mount -t zfs rpool/archlinux/var/lib "${MNT}"/var/lib
-# mount -t zfs rpool/archlinux/var/log "${MNT}"/var/log
+zfs create -o mountpoint=/boot bpool/archlinux/root
+
+zfs list
+
+# Setting ZFS cache
+echo -e "\n${GRN}Setting ZFS cache...${NC}\n"
+
+mkdir -p  "${MNT}"/etc/zfs
+zpool set cachefile=/etc/zfs/zpool.cache rpool
+zpool set cachefile=/etc/zfs/zpool.cache bpool
+cp /etc/zfs/zpool.cache "${MNT}"/etc/zfs/zpool.cache
 
 # Format and mount ESP
 echo -e "\n${GRN}Format and mount ESP...${NC}\n"
 
 for i in ${DISK}; do
- mkfs.vfat -n EFI "${i}"-part1
- mkdir -p "${MNT}"/boot/efis/"${i##*/}"-part1
- mount -t vfat -o iocharset=iso8859-1 "${i}"-part1 "${MNT}"/boot/efis/"${i##*/}"-part1
+ mkfs.vfat -n EFI "${i}"-part2
+ mkdir -p "${MNT}"/boot/efis/"${i##*/}"-part2
+ mount -t vfat -o iocharset=iso8859-1 "${i}"-part2 "${MNT}"/boot/efis/"${i##*/}"-part2
 done
 
 mkdir -p "${MNT}"/boot/efi
-mount -t vfat -o iocharset=iso8859-1 "$(echo "${DISK}" | sed "s|^ *||"  | cut -f1 -d' '|| true)"-part1 "${MNT}"/boot/efi
+mount -t vfat -o iocharset=iso8859-1 "$(echo "${DISK}" | sed "s|^ *||"  | cut -f1 -d' '|| true)"-part2 "${MNT}"/boot/efi
 
 # Generate fstab:
 echo -e "\n${GRN}Generate fstab...${NC}\n"
 
-mkdir "${MNT}"/etc
-genfstab -t PARTUUID "${MNT}" \
-| grep -v swap \
-| sed "s|vfat.*rw|vfat rw,x-systemd.idle-timeout=1min,x-systemd.automount,noauto,nofail|" \
-> "${MNT}"/etc/fstab
+# mkdir "${MNT}"/etc
+genfstab -U -p "${MNT}" >> "${MNT}"/etc/fstab
 
-# # Chroot
-# for i in /dev /proc /sys; do mkdir -p "${MNT}"/"${i}"; mount --rbind "${i}" "${MNT}"/"${i}"; done
-# chroot "${MNT}" /usr/bin/env DISK="${DISK}" bash
+# remove rpool and bpool mounts form fstab
+sed -i '/rpool/d' "${MNT}"/etc/fstab
+sed -i '/bpool/d' "${MNT}"/etc/fstab
+# remove first empty lines from fstab
+sed -i '/./,$!d' "${MNT}"/etc/fstab
+
+cat "${MNT}"/etc/fstab
 
 # Pacstrap packages to MNT
 echo -e "\n${GRN}Pacstrap packages to /mnt...${NC}\n"
 
-pacstrap "${MNT}" base base-devel linux linux-headers linux-firmware grub efibootmgr nano micro openssh ansible git
+pacstrap "${MNT}" base base-devel linux linux-headers linux-firmware grub efibootmgr openssh
 cp /etc/resolv.conf "${MNT}"/etc/resolv.conf
 
 echo -e "\n${GRN}Copy chroot-zfs-script to /mnt...${NC}\n"
 
 cp ./chroot-zfs-script.sh /mnt/root
 
-echo -e "\n${GRN}Run chroot-zfs-script...${NC}\n"
+echo -e "\n${BBLU}Run chroot-zfs-script...${NC}\n"
 
 arch-chroot "${MNT}" /usr/bin/env DISK="${DISK}" sh /root/chroot-zfs-script.sh
 
 echo -e "\n${GRN}Cleanup...${NC}\n"
 
 rm /mnt/root/chroot-zfs-script.sh 
-echo -e "\n${ORG}Run umount -Rl /mnt${NC}\n"
-echo -e "\n${ORG}Run zpool export -a${NC}\n"
+
+echo -e "\n${BRED}Run swapoff -a${NC}"
+echo -e "\n${BRED}Run umount -Rl /mnt${NC}"
+echo -e "\n${BRED}Run zpool export -a${NC}"
+# swapoff -a
 # umount -Rl "${MNT}"
 # zpool export -a
 
